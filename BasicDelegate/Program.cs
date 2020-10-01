98a68be797d153c06d22c2f3b09cb2d39d15afcf
kimhyesuu <you@example.com>
2020-10-01 12:08:01 +0900

    Three

---
+++ BasicDelegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//델리게이트는 인스턴스가 아니라 형식type입니다.
+namespace BasicDelegate
+{
+   class Program
+   {
+      delegate int MyDelegate(int a, int b);
+
+      class Calculator
+      {
+         public int Plus(int a, int b)
+         {
+            return a + b;
+         }
+
+         public static int Minus(int a, int b)
+         {
+            return a - b;
+         }
+      }
+
+      static void Main(string[] args)
+      {
+         Calculator calc = new Calculator();
+         MyDelegate Callback;
+
+         Callback = new MyDelegate(calc.Plus);
+         Console.WriteLine(Callback(3,4));
+      }
+   }
+}
---
+++ ConstaubtsOnTypeParameters/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstaubtsOnTypeParameters
+{
+   class StructArray<T> where T:struct
+   {
+      public T[] Array { get; set; }
+      public StructArray(int size)
+      {
+         Array = new T[size];
+      }
+   }
+
+   class RefArray<T> where T:class
+   {
+      public T[] Array { get; set; }
+      public RefArray(int size)
+      {
+         Array = new T[size];
+      }
+   }
+
+   class Base { }
+   class Derived : Base { }
+   class BaseArray<U> where U : Base
+   {
+      public U[] Array { get; set; }
+      public BaseArray(int size)
+      {
+         Array = new U[size];
+      }
+
+      public void CopyArray<T>(T[] Source) where T : U
+      {
+         Source.CopyTo(Array , 0);
+      }
+   }
+
+   class Program
+   {
+
+      public static T CreateInstance<T>() where T : new()
+      {
+         return new T();
+      }
+
+      static void Main(string[] args)
+      {
+         StructArray<int> a = new StructArray<int>(3);
+         for (int i = 0; i < 3; i++)
+            a.Array[i] = i;
+
+         //이해가 안댐
+         RefArray<StructArray<double>> b = new RefArray<StructArray<double>>(3);
+         for (int i = 0; i < 3; i++)
+         {
+            b.Array[i] = new StructArray<double>(i);
+         }
+
+         BaseArray<Base> c = new BaseArray<Base>(3);
+         c.Array[0] = new Base();
+         c.Array[1] = new Derived();
+         c.Array[2] = CreateInstance<Base>();
+
+         BaseArray<Derived> d = new BaseArray<Derived>(3);
+         d.Array[0] = new Derived();
+         d.Array[1] = CreateInstance<Derived>();
+         d.Array[2] = CreateInstance<Derived>();
+
+
+         for(int i = 0; i < 3; i++)
+         {
+            Console.Write(a.Array[i] + " ");
+         }
+         Console.WriteLine();
+         for (int i = 0; i < 3; i++)
+         {
+            Console.Write(b.Array[i] + " ");
+         }
+         Console.WriteLine();
+         for (int i = 0; i < 3; i++)
+         {
+            Console.Write(c.Array[i] + " ");
+         }
+         Console.WriteLine();
+         for (int i = 0; i < 3; i++)
+         {
+            Console.Write(d);
+         }
+         Console.WriteLine();
+      }
+   }
+}
