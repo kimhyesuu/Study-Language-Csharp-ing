8a33d36eef824d475200f662f691f767f1594f11
kimhyesu <juid00@naver.com>
2020-10-06 22:32:53 +0900

    wpfsyntax

---
+++ Wpf/wpfTest/ListViewWithDataGridAdded/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListViewWithDataGridAdded
+{
+   /// <summary>
+   /// MainWindow.xaml에 대한 상호 작용 논리
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+
+      List<User> items = new List<User>();
+
+      public MainWindow()
+      {
+         InitializeComponent();     
+         items.Add(new User() { Name = "John Doe", Age = 42, Mail = "john@doe-family.com" });
+         items.Add(new User() { Name = "Jane Doe", Age = 39, Mail = "jane@doe-family.com" });
+         items.Add(new User() { Name = "Sammy Doe", Age = 7, Mail = "sammy.doe@gmail.com" });
+         lvUsers.ItemsSource = items;
+      }
+   }
+
+   public partial class User
+   {
+      public string Name { get; set; }
+
+      public int Age { get; set; }
+
+      public string Mail { get; set; }
+   }
+}
---
+++ Wpf/wpfTest/datatemplete/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace datatemplete
+{
+   /// <summary>
+   /// MainWindow.xaml에 대한 상호 작용 논리
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      Person src = new Person { Name = "Ali", Age = 27 };
+      List<Person> people = new List<Person>();
+
+      public MainWindow()
+      {
+         InitializeComponent();
+         people.Add(src);
+         people.Add(new Person { Name = "Mike", Age = 62 });
+         people.Add(new Person { Name = "Brian", Age = 12 });
+         this.DataContext = people;
+      }
+
+      private void Button_Click(object sender, RoutedEventArgs e)
+      {
+         string message = src.Name + " is " + src.Age;
+         MessageBox.Show(message);
+      }
+   }
+
+   public class Person
+   {
+      private string nameValue;
+
+      public string Name
+      {
+         get { return nameValue; }
+         set { nameValue = value; }
+      }
+
+      private double ageValue;
+
+      public double Age
+      {
+         get { return ageValue; }
+         set
+         {
+            if (value != ageValue)
+            {
+               ageValue = value;
+            }
+
+         }
+      }
+   }
+
+}
+
+
