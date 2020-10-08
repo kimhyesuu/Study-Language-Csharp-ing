b05d69e8fb8cb6c2f806e079f507a0637376f9f4
kimhyesu <juid00@naver.com>
2020-10-08 23:11:32 +0900

    Update1008

---
+++ Wpf/Prism.Demo/HS.Prism/BootStrapper.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using HS.Prism.Views;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using Unity;
+
+namespace HS.Prism
+{
+   public class BootStrapper : UnityBootstrapper
+   {
+      public override void Run(bool runWithDefaultConfiguration)
+      {
+         base.Run(runWithDefaultConfiguration);
+      }
+
+      //Create the shell
+      //show the shell window
+      protected override DependencyObject CreateShell()
+      {
+         return Container.Resolve<HSView>();
+      }
+
+      protected override void InitializeShell()
+      {
+         //Set Main window for prism
+         App.Current.MainWindow = Shell as Window;
+         App.Current.MainWindow.Show();
+
+      }
+
+      protected override void ConfigureModuleCatalog()
+      {
+         //user once we create a module
+         base.ConfigureModuleCatalog();
+      }
+   }
+}
---
+++ Wpf/Prism.Demo/SampleModule/Module.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace SampleModule
+{
+    class Module : IModule
+    {
+      private IRegionManager _regionManager;
+
+      public Module(IRegionManager regionManager)
+      {
+         this._regionManager = regionManager;
+      }
+
+      public OnInitialized()
+      {
+
+      }
+   }
+}
