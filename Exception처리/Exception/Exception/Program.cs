2901abcd7729fde900d1362aecf75913f42a29cf
kimhyesu <juid00@naver.com>
2020-11-25 18:44:26 +0900

    Update study on exception

---
+++ Exception처리/Exception/Exception/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+   //System.Exception 클래스는 모든 예외의 조상
+   // 1. ArgumentNullException
+   // 2. IndexOutOfRangeException
+   // 3. 
+   class Program
+   {
+      static void Main(string[] args)
+      {
+        
+         Console.ReadKey();
+      }
+
+      private static void IndexExceptionEx()
+      {
+         int[] array = new[] { 1, 2, 3 };
+         int index = 4;
+         int value = array[index >= 0 && index < 3 ? index : throw new IndexOutOfRangeException()];
+      }
+
+      private static void ExpressionException()
+      {
+         int? a = null;
+         int b = a ?? throw new ArgumentNullException();
+      }
+
+      private static void MultiFunctionException()
+      {
+         int arg = 9;
+         try
+         {
+            DoSomething(1);
+            DoSomething(2);
+            DoSomething(3);
+            DoSomething(4);
+            DoSomething(13);
+            DoSomething(14);
+         }
+         catch (Exception e)
+         {
+            Console.WriteLine(e.Message);
+         }
+      }
+
+      private static void FunctionException()
+      {
+         int arg = 9;
+         try
+         {
+            DoSomething(arg);
+         }
+         catch (Exception e)
+         {
+            Console.WriteLine(e.Message);
+         }
+      }
+
+      private static void DoSomething(int arg)
+      {
+         if (arg < 10)
+            Console.WriteLine(arg);
+         else
+            throw new Exception("arg가 10보다 큽니다.");
+      }
+
+      private static void ThrowExe1()
+      {
+         try
+         {
+            throw new Exception("예외를 던집니다.");
+         }
+         catch (Exception e)
+         {
+            Console.WriteLine(e.Message);
+         }
+      }
+
+      private static void Ex1()
+      {
+         int[] arr = { 1, 2, 3 };
+
+         try
+         {
+            for (int i = 0; i < 5; i++)
+            {
+               Console.WriteLine(arr[i]);
+            }
+
+         }
+         //IndexOutOfRangeException의 조상은 Exception이기때문에 다 받을 수 있음
+         //Exception은 모든 예외를 받는 것이 아니기때문에 방심은 금물
+         catch (Exception e)
+         {
+            Console.WriteLine(e.Message);
+         }
+      }
+   }
+}
---
+++ Exception처리/Exception/UsingFinally/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingFinally
+{
+   // 예외처리에선 return이 오든간에 finally절은 무조건 넘겨준다. 
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         try
+         {
+            Console.WriteLine("제수를 입력하세요. :");
+            string temp = Console.ReadLine();
+            int divisor = Convert.ToInt32(temp);
+
+            Console.WriteLine("피제수를 입력하세요. : ");
+            temp = Console.ReadLine();
+            int divided = Convert.ToInt32(temp);
+
+            Console.WriteLine($"{divisor}/{divided} = {Divide(divisor,divided)}");       
+         }
+         catch(FormatException e)
+         {
+            Console.WriteLine(e.Message);
+         }
+         catch(DivideByZeroException e)
+         {
+            Console.WriteLine(e.Message);
+         }
+         finally
+         {
+            Console.WriteLine("프로그램을 종료합니다.");
+         }
+
+         Console.ReadKey();
+      }
+
+      private static object Divide(int divisor, int divided)
+      {
+         try
+         {
+            Console.WriteLine("Divide Function 시작");
+            return divisor / divided;
+         }
+         catch(DivideByZeroException e)
+         {
+            Console.WriteLine("Divide Function 예외 발생");
+            throw e;
+         }
+         finally
+         {
+            Console.WriteLine("Divide Function 끝");
+         }
+      }
+   }
+}
